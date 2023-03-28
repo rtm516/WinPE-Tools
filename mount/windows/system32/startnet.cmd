cba0319da221f5292db0e27fac52809d25b9c056
rtm516 <rtm516@users.noreply.github.com>
2023-03-28 12:05:43 +0100

    Add system drive detection

--- mount/windows/system32/startnet.cmd
+++ mount/windows/system32/startnet.cmd
@@ -5,4 +5,9 @@ echo Welcome to the WinPE tools image
 echo Starting...
 wpeinit
 
+echo Locating system drive
+@REM Drive X is missing as thats the one WinPE boots from
+for %%a in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do @if exist %%a:\Windows\ set SYSTEMDRIVE=%%a
+echo Located %SYSTEMDRIVE% as system drive ($env:SYSTEMDRIVE)
+
 powershell -NoLogo
